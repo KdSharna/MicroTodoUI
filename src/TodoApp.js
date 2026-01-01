0e19fb0e95461f924cebab75274bb5540a0deffb
kdsharma <sharmakapil79027@gmail.com>
2026-01-01 20:53:47 +0530

    Update TodoApp.js

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -6,9 +6,9 @@ import { Box } from '@mui/material';
 import config from './config'; // Import the config file with environment variable support
 
 // Use the config to define API URLs
-const GET_TASKS_API_BASE_URL = "http://10.0.214.42";
-const DELETE_TASK_API_BASE_URL = "http://10.0.92.235";
-const CREATE_TASK_API_BASE_URL = "http://10.0.167.184";
+const GET_TASKS_API_BASE_URL = "http://10.0.214.42/";
+const DELETE_TASK_API_BASE_URL = "http://10.0.92.235/";
+const CREATE_TASK_API_BASE_URL = "http://10.0.167.184/";
 
 //Update Task Functionality is Work In Progress
 // const UPDATE_TASK_API_BASE_URL = '';
